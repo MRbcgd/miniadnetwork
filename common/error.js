7886adbea4a3af9c8e5675eaf1a0a33bb9c8dea7
MRbcgd <quittypig@gmail.com>
2018-09-22 20:25:36 +0900

    sync equip, error file with core

--- common/error.js
+++ common/error.js
@@ -1,7 +1,7 @@
 /*
     author     : bak chulhyong
     created    : 2018 - 09 - 16
-    modified   : 2018 - 09 - 16
+    modified   : 2018 - 09 - 20
     description: error file
 */
 module.exports.error = {
@@ -9,7 +9,13 @@ module.exports.error = {
     //ERROR
     "ITN_SVR_ERR" : "1001", //INTERNAL SERVER ERROR
     "BYTE_LEN_ERR": "1002", //BYTE LENGTH ERROR
+    "INVALID_URL" : "1003", //INVALID URL
     // TargetList
-    "TGL_EMPTY" : "1100",
+    "TGL_EMPTY" : "1100", //tbATargetList EMPTY
+    // Advert
+    "INVALID_CTT_KEY" : "1200", //INVALID CONTENT KEY
+    "INVALID_ART_KIND": "1201", //INVALID ART KIND
+    "CTT_NOT_PLAYING" : "1202", //CONTENT STATUS NOT PLAYING
+    "ADV_AVAIL_EMPTY" : "1203", //EMPTY AVAIL AVAVAILD
 
 }
